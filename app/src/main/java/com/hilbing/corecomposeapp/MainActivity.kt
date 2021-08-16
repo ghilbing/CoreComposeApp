570220f52044769bc35b69051778835fc9c33618
Gretel Hilbing <ghilbing@gmail.com>
2021-08-16 14:52:37 -0700

    Align composables

--- app/src/main/java/com/hilbing/corecomposeapp/MainActivity.kt
+++ app/src/main/java/com/hilbing/corecomposeapp/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +29,16 @@ fun MainScreen(){
     Surface(
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()) {
+        Surface(
+            color = Color.Gray,
+            modifier = Modifier.wrapContentSize(
+                align = Alignment.BottomStart
+            )
+        ) {
+            Text(
+                text = "Wrapped content",
+                style = MaterialTheme.typography.h4)
+        }
 
     }
 }
